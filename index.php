9cc2c70607cdf11a2ae70d9eb059a3d65c8311ed
Jenil Calcuttawala <jenilcalcuttawala@gmail.com>
2016-09-21 19:59:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -22,9 +22,13 @@
   <title>Twitter rtCampAssignment</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+   <script src="js/bootstrap.min.js"></script>
+   /*
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  */
   <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
   <style>
