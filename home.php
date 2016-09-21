3d705bddf65bef9680653443dfec40f6ff9cef77
jenil777007 <jenilcalcuttawala@gmail.com>
2016-09-21 20:24:54 +0530

    checkpoint 1

--- home.php
+++ home.php
@@ -106,14 +106,14 @@
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Home</a></li>
       <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li> 
+      <li><a href="#">Page 2</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
     </ul>
   </div>
 </nav>
-  
+
 <div class="container">
 
     <div class="row">
